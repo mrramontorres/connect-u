f4c6f73da17bd2c16e7431eb6b1da4e5f6241c4a
Thoko <thokozile.soko@yahoo.com>
2021-02-22 20:14:24 -0500

    Added a get function to get the startup to render on the screen.

--- routes/su-api-routes.js
+++ routes/su-api-routes.js
@@ -1,23 +1,23 @@
 // Dependencies
 
 // Requiring our models
-const db = require('../models/startup.js');
+const db = require('../models');
 
 // Routes
 module.exports = (app) => {
   app.get('/api/posts', (req, res) => {
     console.log("su-api-routes activated")
-/*    const query = {};
-    if (req.query.author_id) {
-      query.AuthorId = req.query.author_id;
+   const query = {};
+    if (req.query.startup_id) {
+      query.StartupId = req.query.startup_id;
     }
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Author
-    db.Post.findAll({
+    db.Startup.findAll({
       where: query,
-      include: [db.Author],
-    }).then((dbPost) => res.json(dbPost));
-*/
+      include: [db.Startup],
+    }).then((dbStartup) => res.json(dbStartup));
+
   });
 }
