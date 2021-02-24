454c7c81156923a1a13e78e2d8b0c1bacf393791
Viviana <vivianaaarenas@gmail.com>
2021-02-23 21:32:28 -0500

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,26 +1,28 @@
 // Dependencies
-const path = require('path');
+const path = require("path");
 
 // Routes
 module.exports = (app) => {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get('/', (req, res) =>
-  console.log('GET index not read yet')
-  //  res.sendFile(path.join(__dirname, '../public/blog.html'))
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/blog.html"))
+  );
+
+  app.get("/blog", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/blog.html"))
   );
 
   // second route loads second.html
-  app.get('/second', (req, res) =>
-  console.log('GET second html not read yet')
-  //  res.sendFile(path.join(__dirname, '../public/cms.html'))
+  app.get("/cms", (req, res) =>
+    //console.log("GET second html not read yet")
+    res.sendFile(path.join(__dirname, "../public/cms.html"))
   );
 
   // third route loads third.html
-  app.get('/third', (req, res) =>
-  console.log('GET third html not read yet')
-  //  res.sendFile(path.join(__dirname, '../public/cms.html'))
+  app.get("/authors", (req, res) =>
+    //console.log("GET third html not read yet")
+    res.sendFile(path.join(__dirname, "../public/author-manager.html"))
   );
-
 };
