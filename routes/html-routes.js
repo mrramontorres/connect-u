d36c1ea466625f034ceda9906ff2b8f57edbf1c5
Thoko <thokozile.soko@yahoo.com>
2021-02-25 19:39:52 -0500

    hopefully this is my last time fixing this conflict.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,13 +8,10 @@ module.exports = (app) => {
   // index route loads view.html
   app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "../public/blog.html"))
-<<<<<<< HEAD
-=======
   );
 
   app.get("/blog", (req, res) =>
     res.sendFile(path.join(__dirname, "../public/blog.html"))
->>>>>>> a6af3a3596a3be0702f2149b9c4fff62b35ddfb2
   );
 
   app.get("/blog", (req, res) =>
@@ -29,11 +26,7 @@ module.exports = (app) => {
   );
 
   // third route loads third.html
-<<<<<<< HEAD
-  app.get("/authors", (req, res) =>
-=======
   app.get("/startups", (req, res) =>
->>>>>>> 188afa69e7fd58b8712f032671588000c91dfc17
     //console.log("GET third html not read yet")
     res.sendFile(path.join(__dirname, "../public/author-manager.html"))
   );
