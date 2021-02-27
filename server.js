0608f5e9606105aacf32755a2786c3fdf10e5e70
Wendy Fgigueroa <camomila444@gmail>
2021-02-27 17:14:37 -0500

    added logo images and revised table at browser

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(express.json());
 // Static directory
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Set Handlebars.
 const exphbs = require('express-handlebars');
